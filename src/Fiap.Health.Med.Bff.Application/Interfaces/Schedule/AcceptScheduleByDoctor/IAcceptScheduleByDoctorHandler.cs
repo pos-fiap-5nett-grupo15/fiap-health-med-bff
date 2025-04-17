2602638d983b32dd2a192d093dc722b4a33eb3b7
Evandro <evandroprates15@gmail.com>
2025-04-17 11:42:16 -0300

    Conexão com a rota de aceite de agenda

---
+++ src/Fiap.Health.Med.Bff.Application/Interfaces/Schedule/AcceptScheduleByDoctor/IAcceptScheduleByDoctorHandler.cs
@@ -0,0 +1,10 @@
+﻿using Fiap.Health.Med.Bff.Application.Common;
+using Fiap.Health.Med.Bff.Application.DTOs.Schedule.AcceptScheduleByDoctor;
+
+namespace Fiap.Health.Med.Bff.Application.Interfaces.Schedule.AcceptScheduleByDoctor
+{
+    public interface IAcceptScheduleByDoctorHandler
+    {
+        Task<Result> HandlerAsync(AcceptScheduleByDoctorHandlerRequest request, CancellationToken ct);
+    }
+}
--- src/Fiap.Health.Med.Bff.Infrastructure/Services/IScheduleManagerService.cs
+++ src/Fiap.Health.Med.Bff.Infrastructure/Services/IScheduleManagerService.cs
@@ -5,5 +5,6 @@ namespace Fiap.Health.Med.Bff.Infrastructure.Http.Services
     public interface IScheduleManagerService
     {
         Task<BaseServiceResponse> DeclineScheduleByIdAsync(long scheduleId, int doctorId, CancellationToken ct);
+        Task<BaseServiceResponse> AcceptScheduleByIdAsync(long scheduleId, int doctorId, CancellationToken ct);
     }
 }
