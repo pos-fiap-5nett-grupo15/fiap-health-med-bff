1dca30df29e85def85393744afa3c87e10542701
dev-guilhCASTRO <guilherme.cbpereira@gmail.com>
2025-05-11 20:52:20 -0300

    Added load tests

---
+++ tests/Fiap.Health.Med.Bff.LoadTests/UseCases/v1/Doctor/get-doctor-by-id.js
@@ -0,0 +1,23 @@
+﻿import http from 'k6/http';
+import { check, sleep, fail } from 'k6';
+import doctorAuth from './UseCases/v1/doctorAuth.js';
+
+export default function () {
+    let headers = {
+        'Authorization': doctorAuth,
+        'Content-Type': 'application/json',
+        'Content-Type': 'application/json',
+    };
+    let res = http.get(`http://135.237.26.77/Doctor/1`, { headers });
+
+    if (
+        !check(res, {
+            'status é 200': (r) => r.status === 200,
+            //'tempo de resposta < 500ms': (r) => r.timings.duration < 500,
+        })
+    ) {
+        fail(`get-doctor-by-filters - K6 Check - Fail: status code recebido - ${res.status}`);
+    }
+
+    sleep(1); // pausa de 1s entre as execuções
+}
